5f9e4fb949ff5bf0265ccd46fec7bb89378ce419
rajatp799 <rajatpradhan799@gmail.com>
2024-11-25 13:51:58 +0530

    01_variables.js

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 15544
+let accountEmail = "rajatyj67@gmail.com"
+var accountPassowrd = "12345"
+accountCity = "odisha"
+let accountState;
+
+// accountId = 2 // not allowed/ not exicute
+
+console.log(accountId);
+
+accountEmail = "hc@hc.com"
+accountPassowrd = "23466"
+accountCity = "bhubaneswar"
+
+
+
+/*
+prefer not use var 
+because of issue in block scope and functional scope
+*/
+
+
+console.table ([accountId , accountEmail , accountPassowrd , accountCity , accountState])
