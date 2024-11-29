fcfb184e4e149288efeec510104561ee4ca498e4
rajatp799 <rajatpradhan799@gmail.com>
2024-11-29 13:26:46 +0530

    ConversionOperation

--- 01-basics/03_conversionOperation.js
+++ 01-basics/03_conversionOperation.js
@@ -20,11 +20,6 @@
 
     // 1 => true; 0 =>false
     // "" => false
-    // "Rajat" => true
-
-    let someNumber = 33
-    let stringNumber = String(someNumber)
-    console.log(stringNumber);    
-    console.log(typeof stringNumber);    
+    // "Rajat" => true   
 
         
