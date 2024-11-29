61f3d350d893723aae76f1d20dec839cfd511e09
rajatp799 <rajatpradhan799@gmail.com>
2024-11-29 13:29:02 +0530

    Revert "ConversionOperation"
    This reverts commit fcfb184e4e149288efeec510104561ee4ca498e4.

--- 01-basics/03_conversionOperation.js
+++ 01-basics/03_conversionOperation.js
@@ -20,6 +20,11 @@
 
     // 1 => true; 0 =>false
     // "" => false
-    // "Rajat" => true   
+    // "Rajat" => true
+
+    let someNumber = 33
+    let stringNumber = String(someNumber)
+    console.log(stringNumber);    
+    console.log(typeof stringNumber);    
 
         
