f9c5496af1c1ebd70eaafa4c278e09243ba2b73d
rajatp799 <rajatpradhan799@gmail.com>
2024-11-30 18:24:15 +0530

    practice.js

---
+++ 01-basics/practice.js
@@ -0,0 +1,13 @@
+// let a = "rajat"
+
+// //let stringNumber = String(someNumber)
+
+// let booleanIsLoggedIn = Boolean (a)
+
+// console.log(booleanIsLoggedIn);
+// console.log(typeof (booleanIsLoggedIn));
+
+
+for (let i=1; i<=1000; i++){
+        console.log(i)
+}
